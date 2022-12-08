ef3d0776bf345b84a9397597d94b3f431a76a3b7
nikolainiklaus <118966948+nikolainiklaus@users.noreply.github.com>
2022-12-08 15:22:12 +0100

    hw plus extras

---
+++ script.js
@@ -0,0 +1,114 @@
+let numbersUsed = [];
+
+function onloadOperations() {
+  let boardNode = document.getElementById("bingo-board");
+  let cellNumber = 76;
+
+  for (let cell = 1; cell <= cellNumber; cell++) {
+    let newCell = document.createElement("div");
+    newCell.classList.add("bingo-cell");
+    newCell.innerText = cell;
+
+    boardNode.appendChild(newCell);
+  }
+}
+
+function randomizeNumber() {
+  let randomNumber = Math.floor(Math.random() * 76) + 1;
+  let randomColor = randomizeColor();
+
+  if (numbersUsed.includes(randomNumber)) {
+    console.log("number already used");
+  } else {
+    displayNumber(randomNumber, randomColor);
+    displayUserBoardNumber(randomNumber, randomColor);
+    numbersUsed.push(randomNumber);
+    console.log(numbersUsed);
+  }
+}
+
+function displayNumber(randomNumber, randomColor) {
+  let cellNodes = document.getElementsByClassName("bingo-cell");
+
+  for (cell of cellNodes) {
+    if (randomNumber == cell.innerText) {
+      cell.style.backgroundColor = randomColor;
+      console.log(randomNumber, randomColor);
+    }
+  }
+}
+
+function randomizeColor() {
+  function randomInteger(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+
+  function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r, g, b];
+  }
+
+  function randomHexColor() {
+    let [r, g, b] = randomRgbColor();
+
+    let hr = r.toString(16).padStart(2, "0");
+    let hg = g.toString(16).padStart(2, "0");
+    let hb = b.toString(16).padStart(2, "0");
+
+    return "#" + hr + hg + hb;
+  }
+
+  let hex = randomHexColor();
+  return hex;
+}
+
+function createUserBoards() {
+  let userBoardsAmount = document.getElementById("userBoardsAmount").value;
+  let userBoardsNumber = parseInt(userBoardsAmount);
+
+  if (userBoardsAmount === "" || userBoardsAmount < 1) {
+    alert("Please enter a number greater than 0");
+  } else {
+    displayUserBoards(userBoardsNumber);
+  }
+}
+
+function displayUserBoards(userBoardsNumber) {
+  let userBoardsNode = document.getElementById("user-boards");
+
+  if (userBoardsNode.childElementCount > 0) {
+    userBoardsNode.innerHTML = "";
+  }
+
+  for (let userBoards = 1; userBoards <= userBoardsNumber; userBoards++) {
+    let newBoard = document.createElement("div");
+    newBoard.classList.add("bingoboard");
+    userBoardsNode.appendChild(newBoard);
+    console.log(newBoard);
+
+    for (let cell = 1; cell <= 24; cell++) {
+      let newCell = document.createElement("div");
+      let randomNumber = Math.floor(Math.random() * 76) + 1;
+
+      newCell.classList.add("bingo-cell");
+      newCell.innerText = randomNumber;
+
+      newBoard.appendChild(newCell);
+    }
+  }
+}
+
+function displayUserBoardNumber(randomNumber, randomColor) {
+  let cellNodes = document.getElementsByClassName("bingo-cell");
+
+  for (cell of cellNodes) {
+    if (randomNumber == cell.innerText) {
+      cell.style.backgroundColor = randomColor;
+      console.log(randomNumber, randomColor);
+    }
+  }
+}
+
+window.onload = onloadOperations;
